a19347cecb9991fc3983cdd47edaac0db14e5a3f
Jakob Frank <jakob.frank@redlink.co>
2017-05-23 11:24:18 +0200

    solrlib goes open-source! (Apache 2.0)

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # SolrLib
-_an easy to use solr client, that works embedded or with SolrCloud._
+_an easy to use solr client, that works embedded or with SolrServer and SolrCloud._
--- spring-boot/src/main/java/io/redlink/solrlib/spring/boot/autoconfigure/SolrLibHealthIndicatorConfiguration.java
+++ spring-boot/src/main/java/io/redlink/solrlib/spring/boot/autoconfigure/SolrLibHealthIndicatorConfiguration.java
@@ -1,5 +1,17 @@
 /*
- * Copyright (c) 2017 Redlink GmbH.
+ * Copyright 2017 redlink GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package io.redlink.solrlib.spring.boot.autoconfigure;
 
@@ -17,7 +29,6 @@ import org.springframework.context.annotation.Configuration;
 import java.util.Set;
 
 /**
- * Created by jakob on 12.04.17.
  */
 @Configuration
 @AutoConfigureAfter({SolrLibEmbeddedAutoconfiguration.class, SolrLibStandaloneAutoconfiguration.class, SolrLibCloudAutoconfiguration.class})
